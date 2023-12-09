3dc69325c1337f24ab4e8d75f61fa475fcfa078c
Suyono <suyono3484@gmail.com>
2023-12-09 10:42:05 +0000

    test: add more cron

---
+++ docker/alpine/etc/wingmate/crontab.d/cron2.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+export WINGMATE_DUMMY_PATH=/usr/local/bin/wmdummy
+export WINGMATE_LOG=/var/log/cron2.log
+export WINGMATE_LOG_MESSAGE="cron scheduled using 17,42 */2 * * *"
+
+exec /usr/local/bin/wmoneshot
