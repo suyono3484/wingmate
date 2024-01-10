3dbac84f36db39b44a0fbe54a8bee79c622d7502
Suyono <suyono3484@gmail.com>
2024-01-11 09:06:11 +1100

    wip: add version command to exec

--- cmd/exec/exec.go
+++ cmd/exec/exec.go
@@ -1,43 +1,61 @@
 package main
 
 import (
+	_ "embed"
 	"errors"
 	"fmt"
+	"gitea.suyono.dev/suyono/wingmate"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"golang.org/x/sys/unix"
 	"log"
 	"os"
 	"os/exec"
 	"strconv"
 	"strings"
-
-	"gitea.suyono.dev/suyono/wingmate"
-	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
-	"golang.org/x/sys/unix"
 )
 
+type execApp struct {
+	childArgs []string
+	err       error
+}
+
 const (
-	setsidFlag = "setsid"
-	EnvSetsid  = "SETSID"
-	userFlag   = "user"
-	EnvUser    = "USER"
+	setsidFlag  = "setsid"
+	EnvSetsid   = "SETSID"
+	userFlag    = "user"
+	EnvUser     = "USER"
+	versionFlag = "version"
 )
 
 var (
-	rootCmd = &cobra.Command{
-		Use:  "wmexec",
-		RunE: execCmd,
-	}
 
-	childArgs []string
+	//go:embed version.txt
+	version string
 )
 
 func main() {
 	var (
-		found    bool
-		i        int
-		arg      string
-		selfArgs []string
+		selfArgs   []string
+		childArgs  []string
+		app        *execApp
+		rootCmd    *cobra.Command
+		versionCmd *cobra.Command
+		err        error
 	)
+
+	app = &execApp{}
+
+	rootCmd = &cobra.Command{
+		Use:          "wmexec",
+		SilenceUsage: true,
+		RunE:         app.execCmd,
+	}
+
+	versionCmd = &cobra.Command{
+		Use:  "version",
+		RunE: app.versionCmd,
+	}
 
 	rootCmd.PersistentFlags().BoolP(setsidFlag, "s", false, "set to true to run setsid() before exec")
 	viper.BindPFlag(EnvSetsid, rootCmd.PersistentFlags().Lookup(setsidFlag))
@@ -45,35 +63,20 @@ func main() {
 	rootCmd.PersistentFlags().StringP(userFlag, "u", "", "\"user:[group]\"")
 	viper.BindPFlag(EnvUser, rootCmd.PersistentFlags().Lookup(userFlag))
 
+	rootCmd.PersistentFlags().Bool(versionFlag, false, "print version")
+	viper.BindPFlag(versionFlag, rootCmd.PersistentFlags().Lookup(versionFlag))
+
 	viper.SetEnvPrefix(wingmate.EnvPrefix)
 	viper.BindEnv(EnvUser)
 	viper.BindEnv(EnvSetsid)
 	viper.SetDefault(EnvSetsid, false)
 	viper.SetDefault(EnvUser, "")
 
-	found = false
-	for i, arg = range os.Args {
-		if arg == "--" {
-			found = true
-			if len(os.Args) <= i+1 {
-				log.Println("invalid argument")
-				os.Exit(1)
-			}
-			selfArgs = os.Args[1:i]
-			childArgs = os.Args[i+1:]
-			break
-		}
-	}
-	if !found {
-		log.Println("invalid argument")
-		os.Exit(1)
-	}
+	rootCmd.AddCommand(versionCmd)
 
-	if len(childArgs) == 0 {
-		log.Println("invalid argument")
-		os.Exit(1)
-	}
-
+	selfArgs, childArgs, err = argSplit()
+	app.childArgs = childArgs
+	app.err = err
 	rootCmd.SetArgs(selfArgs)
 	if err := rootCmd.Execute(); err != nil {
 		log.Println(err)
@@ -81,7 +84,56 @@ func main() {
 	}
 }
 
-func execCmd(cmd *cobra.Command, args []string) error {
+func argSplit() ([]string, []string, error) {
+	var (
+		i         int
+		arg       string
+		selfArgs  []string
+		childArgs []string
+	)
+	found := false
+	for i, arg = range os.Args {
+		if arg == "--" {
+			found = true
+			if i+1 == len(os.Args) {
+				return nil, nil, errors.New("invalid argument")
+			}
+
+			if len(os.Args[i+1:]) == 0 {
+				return nil, nil, errors.New("invalid argument")
+			}
+
+			selfArgs = os.Args[1:i]
+			childArgs = os.Args[i+1:]
+			break
+		}
+
+		if !found {
+			return nil, nil, errors.New("invalid argument")
+		}
+	}
+	return selfArgs, childArgs, nil
+}
+
+func (e *execApp) versionCmd(cmd *cobra.Command, args []string) error {
+	e.printVersion()
+	return nil
+}
+
+func (e *execApp) printVersion() {
+	fmt.Print(version)
+	os.Exit(0)
+}
+
+func (e *execApp) execCmd(cmd *cobra.Command, args []string) error {
+	if viper.GetBool(versionFlag) {
+		e.printVersion()
+	}
+
+	if e.err != nil {
+		return e.err
+	}
+
 	if viper.GetBool(EnvSetsid) {
 		_, _ = unix.Setsid()
 	}
@@ -119,13 +171,13 @@ func execCmd(cmd *cobra.Command, args []string) error {
 
 	}
 
-	if path, err = exec.LookPath(childArgs[0]); err != nil {
+	if path, err = exec.LookPath(e.childArgs[0]); err != nil {
 		if !errors.Is(err, exec.ErrDot) {
 			return fmt.Errorf("lookpath: %w", err)
 		}
 	}
 
-	if err = unix.Exec(path, childArgs, os.Environ()); err != nil {
+	if err = unix.Exec(path, e.childArgs, os.Environ()); err != nil {
 		return fmt.Errorf("exec: %w", err)
 	}
 
